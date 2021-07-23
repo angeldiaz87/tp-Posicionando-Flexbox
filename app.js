30a8cf3e2ddc7521bb79329360f57af544bb5e0f
angeldiaz87 <kornangel.ad@gmail.com>
2021-07-23 16:31:39 -0300

    agregando formulario y login

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ app.use(express.static('public'));
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
 
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname, 'views','register.html')));
+
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
+
+
 app.listen(port,() => console.log(`servidor corriendo en http://localhost:${port}`) )
